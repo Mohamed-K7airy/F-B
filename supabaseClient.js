90a0d23f7e05a3efc953b9833a02a870dac76515
HAMO <Mohamedkh476@gmail.com>
2026-03-11 16:43:31 +0200

    f

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,6 @@
-// Using a stable CDN that browsers love
-import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://twkbvvinlzdvtqkauvkv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3a2J2dmlubHpkdnRxa2F1dmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NTI0ODksImV4cCI6MjA4ODAyODQ4OX0.PVUSz03RkXKlMky_qf_U5pAvLU7_CBhB1PaRgTWJ4Zk'
+const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_APP_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
